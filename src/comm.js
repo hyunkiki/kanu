44ed43a0c3a203c863d2521c35f614ce12385e03
hyunkiki <rlagusrl98@gmail.com>
2024-05-01 23:50:14 +0900

    05-01 업데이트

--- src/comm.js
+++ src/comm.js
@@ -1,3 +1,4 @@
+//home
 ScrollTrigger.create({
   animation: gsap.from(".logo_img", {
     y: "50vh",
@@ -12,4 +13,23 @@ ScrollTrigger.create({
   markers: false,
   pin: false,
   pinSpacing: false,
+});
+
+//about
+gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.config({
+  ignoreMobileResize: true,
+});
+gsap.to(".about_contents_01 .about_contents_list01 .about_txt_wr .text", {
+  scrollTrigger: {
+    trigger: ".about_contents_01",
+    start: "top 20%",
+    end: "200% top",
+    //		  markers:true,
+    toggleClass: {
+      targets: ".about_contents_01 .about_contents_list01 .about_txt_wr .text",
+      className: "active",
+    },
+    toggleActions: "none none none reset",
+  },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,19 @@ module.exports = {
       },
       height: {
         35: "35px",
+        70: "70vh",
       },
       borderRadius: {
         25: "25%",
+      },
+      zIndex: {
+        1: "1",
+        1000: "1000",
+        9999: "9999",
+      },
+      fontSize: {
+        10: "10vw",
+        50: "50px",
       },
     },
   },

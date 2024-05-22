41b775cdda5364a776b958e327a47ef6a1887453
hyunkiki <rlagusrl98@gmail.com>
2024-05-22 23:15:40 +0900

    05-22 업데이트

--- build/comm.js
+++ build/comm.js
@@ -86,3 +86,28 @@ gsap.to(".about_wrap2 .about_box2", {
 
 //kanu_product
 gsap.registerPlugin(ScrollTrigger);
+const sections = gsap.utils.toArray(".horizontal_box"); //섹션들을 선택한다 , .horizontal_box 클래스를 가진 모든 요소를 선택하여 배열로 저장한다.
+
+let maxWidth = 0; //최대 너비 계산 변수 초기화한다
+
+const getMaxWidth = () => {
+  maxWidth = 0;
+  sections.forEach((section) => {
+    maxWidth += section.offsetWidth;
+  });
+}; //최대 너비 계산 함수, 각 섹션의 너비를 합산하여 maxWidth를 계산하는 함수이다.
+
+getMaxWidth(); //최대 너비 초기 계산, 초기 실행 시 최대 너비를 계산한다
+ScrollTrigger.addEventListener("refreshInit", getMaxWidth); //새로고침 이벤트에 최대 너비 재계산 추가, ScrollTrigger가 새로고침 이벤트를 발생시킬 때마다 최대 너비를 재계산한다.
+
+gsap.to(sections, {
+  x: () => `-${maxWidth - window.innerWidth}`,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizontal_wr",
+    pin: true,
+    scrub: true,
+    end: () => `+=${maxWidth}`, //+=${maxWidth}는 스크롤 길이를 maxWidth만큼 연장한다.
+    invalidateOnRefresh: true,
+  },
+}); //수평 이동 애니메이션 설정

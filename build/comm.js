0104d62d5479daa7df5be31119fef5bb97d06b44
hyunkiki <rlagusrl98@gmail.com>
2024-06-05 22:02:01 +0900

    06-05 업데이트

--- build/comm.js
+++ build/comm.js
@@ -85,3 +85,32 @@ gsap.to(".about_wrap2 .about_box2", {
 });
 
 //kanu_product
+gsap.registerPlugin(ScrollTrigger);
+
+// Sections
+const sections = gsap.utils.toArray(".horizon_box");
+
+let maxWidth = 0;
+
+const getMaxWidth = () => {
+  maxWidth = 0;
+  sections.forEach((section) => {
+    maxWidth += section.offsetWidth;
+  });
+};
+
+getMaxWidth();
+ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
+
+gsap.to(sections, {
+  x: () => `-${maxWidth - window.innerWidth}`,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizon_wrap",
+    pin: true,
+    scrub: true,
+    // markers: true,
+    end: () => `+=${maxWidth}`,
+    invalidateOnRefresh: true,
+  },
+});
